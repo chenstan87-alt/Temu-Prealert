52bc43a84f4e733e1b7734ba1ada62471de05502
chenstan87-alt <chenstan87@gmail.com>
2025-11-22 22:35:50 -0500

    Add files via upload

--- prealert_dashboard.py
+++ prealert_dashboard.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
-from prealert_jfk import get_jfk_data
-from prealert_ord import get_ord_data
-from transfer_prealert_atl import get_atl_data
+from analysis import get_data,get_transfer_data
 
 now = pd.Timestamp.now()
 
@@ -11,14 +9,13 @@ ten_days_ago_start = (now - pd.Timedelta(days=10)).normalize()
 end = yesterday_end.strftime("%Y-%m-%d %H:%M:%S")
 start = ten_days_ago_start.strftime("%Y-%m-%d %H:%M:%S")
 
-jfk_mawb, jfk_container_channel = get_jfk_data(start, end)
-ord_mawb, ord_container_channel = get_ord_data(start, end)
-atl_mawb, atl_container_channel = get_atl_data(start, end)
+ord_mawb, ord_container_channel, jfk_mawb, jfk_container_channel = get_data(start, end)
+ord_atl_mawb, ord_atl_container_channel = get_transfer_data(start, end)
 
 
 jfk_container_channel.dropna(subset=['container_no'],inplace=True)
 ord_container_channel.dropna(subset=['container_no'],inplace=True)
-atl_container_channel.dropna(subset=['container_no'],inplace=True)
+ord_atl_container_channel.dropna(subset=['container_no'],inplace=True)
 
 #overall_data_=overall_data.sort_values(by='操作日期',ascending=False)
 
@@ -41,13 +38,13 @@ st.markdown("---")
 with st.expander("二、两天内超时托盘", expanded=False):
     st.dataframe(ord_container_channel)
 
-st.title("ATL卡转 KPI")
+st.title("ORD卡转ATL KPI")
 
 with st.expander("一、两天内超时主单", expanded=False):
-    st.dataframe(atl_mawb)
+    st.dataframe(ord_atl_mawb)
 
 st.markdown("---")
 with st.expander("二、两天内超时托盘", expanded=False):
-    st.dataframe(atl_container_channel)
+    st.dataframe(ord_atl_container_channel)
 
 
